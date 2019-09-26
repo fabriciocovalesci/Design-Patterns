29d330011d3982e0c477491aea2ed5364e1769f5
fabricio <fabcovalesci@gmail.com>
2019-09-26 23:28:31 +0000

    Finalizando o exemplo, padrao Chain of Responsability

--- Desconto-Impostos/calculador_descontos.py
+++ Desconto-Impostos/calculador_descontos.py
@@ -1,15 +1,36 @@
 # -*- coding:UTF-8 -*-
+"""
+Esses descontos formam como se fosse uma "corrente", ou seja, um ligado ao outro. 
+Daí o nome do padrão de projeto: Chain of Responsibility. 
+A ideia do padrão é resolver problemas como esses: de acordo com o cenário, 
+devemos realizar alguma ação. Ao invés de escrevermos código procedural, 
+e deixarmos um único método descobrir o que deve ser feito, quebramos essas 
+responsabilidades em várias diferentes classes, e as unimos como uma corrente.
+"""
+
+from desconto import Desconto_por_cinco_itens, Desconto_por_mais_quinhentos_reais, Sem_desconto
 
 class Calcula_descontos(object):
     
     def calcula(self, orcamento):
         
-        if orcamento.total_itens > 5:
-            return orcamento.valor * 0.1
+        desconto = Desconto_por_cinco_itens(
+            Desconto_por_mais_quinhentos_reais(Sem_desconto())
+            ).calcula(orcamento)
         
-        elif orcamento.valor > 500:
-            return orcamento.valor * 0.07
-        
+        return desconto
 
-if __name__ =="main":
+if __name__ == "__main__":
+
+    from orcamento import Orcamento, Item
     
+    orcamento = Orcamento()
+    orcamento.adiciona_item(Item('ITEM - 1', 100))
+    orcamento.adiciona_item(Item('ITEM - 2',  50)) 
+    orcamento.adiciona_item(Item('ITEM - 3', 500))
+    orcamento.adiciona_item(Item('ITEM - 4', 400))
+    
+    calculador = Calcula_descontos()
+    desconto = calculador.calcula(orcamento)
+    
+    print(f"Desconto calculado R$ {desconto}")
--- Desconto-Impostos/desconto.py
+++ Desconto-Impostos/desconto.py
@@ -1,19 +1,36 @@
 # -*- coding:UTF-8 -*-
 
+"""
+Se o orçamento atende a regra de um desconto, o mesmo já calcula o desconto. 
+Caso contrário, ele passa para o "próximo" desconto, qualquer que seja esse próximo desconto.
+"""
+
+
 class Desconto_por_cinco_itens(object):
+    
+    def __init__(self, proximo_desconto):
+        self.__proximo_desconto = proximo_desconto
     
     def calcula(self, orcamento):
         
         if orcamento.total_itens > 5:
             return orcamento.valor * 0.1
         else:
-            return 0
+            return self.__proximo_desconto.calcula(orcamento)
         
 class Desconto_por_mais_quinhentos_reais(object):
+    
+    def __init__(self, proximo_desconto):
+        self.__proximo_desconto = proximo_desconto
     
     def calcula(self, orcamento):
         
         if orcamento.valor > 500:
             return orcamento.valor * 0.07
         else:
-            return 0
+            return self.__proximo_desconto.calcula(orcamento)
+        
+class Sem_desconto(object):
+    
+    def calcula(self):
+        return 0
