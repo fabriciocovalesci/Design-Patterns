fbd6d2d8923d4ab05695b2b703a59a51bf28b3ad
fabricio <fabcovalesci@gmail.com>
2019-09-26 18:40:25 +0000

    Alterando impostos.py

--- Orcamento-Impostos/calculador_de_impostos.py
+++ Orcamento-Impostos/calculador_de_impostos.py
@@ -7,16 +7,23 @@ Temos uma regra de negócio na qual os valores dos orçamentos
 podem ser submetidos a alguns impostos, como ISS, ICMS
 
 '''
+from impostos import ISS, INSS
+
 class Calculador_de_impostos(object):
     
-    def realiza_calculo(self, orcamento):
+    def realiza_calculo(self, orcamento, impostos):
         
-        icms_calculado = orcamento.valor * 0.1
-        print(icms_calculado)
+        valor = impostos.calcula(orcamento)
+        print(valor)
         
 if __name__ == "__main__":
     from orcamento import Orcamento
+    from impostos import INSS, ISS
     
     orcamento = Orcamento(500.0)
-    Calculador_de_impostos = Calculador_de_impostos()
-    Calculador_de_impostos.realiza_calculo(orcamento)
+    print("\nImposto de INSS ")
+    calculador_de_impostos = Calculador_de_impostos()
+    calculador_de_impostos.realiza_calculo(orcamento, INSS())
+    print("\nImposto de ISS ")
+    calculador_de_impostos.realiza_calculo(orcamento, ISS())
+  
--- Orcamento-Impostos/impostos.py
+++ Orcamento-Impostos/impostos.py
@@ -1,7 +1,12 @@
 # -*- coding: UTF-8 -*-
 # impostos.py
-def calcula_ICMS(orcamento):
-    return orcamento.valor * 0.1
 
-def calcula_ISS(orcamento):
-    return orcamento.valor * 0.06
+class INSS(object):
+
+    def calcula(self, orcamento):
+        return  orcamento.valor * 0.1
+
+class ISS(object):
+    
+    def calcula(self, orcamento):
+        return orcamento.valor * 0.06
